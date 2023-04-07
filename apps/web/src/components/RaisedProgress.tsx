b44eaac4599b2317fd594fb9eb47e5e80040692b
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-04-07 09:45:18 +0200

    Fix TwitterCard

--- apps/web/src/components/RaisedProgress.tsx
+++ apps/web/src/components/RaisedProgress.tsx
@@ -20,31 +20,23 @@ export const RaisedProgress = ({ funds }: Props) => {
   );
 };
 
-const Wrapper = createComponent(
-  "div",
-  tv({ base: "mb-6 w-full border-2 border-black/50 relative" })
-);
-const ProgressBar = createComponent(
-  "div",
-  tv({
-    base: "absolute h-full bg-gradient-to-r from-[#80FFBB] to-[#E6FF4D]",
-  })
-);
-const Indicator = createComponent(
-  "div",
-  tv({
-    base: "absolute -top-2 h-16 border-2 border-r border-black/50",
-  })
-);
-const MaxValue = createComponent(
-  "div",
-  tv({
-    base: "p-2 text-right text-lg font-bold text-black/50 relative z-10",
-  })
-);
-const CurrentValue = createComponent(
-  "div",
-  tv({
-    base: "absolute top-14 text-lg font-bold text-black/50",
-  })
-);
+export const wrapperStyle =
+  "flex mb-6 w-full border-2 border-black/50 relative";
+
+export const progressBarStyle =
+  "absolute h-full bg-gradient-to-r from-[#80FFBB] to-[#E6FF4D]";
+
+export const indicatorStyle =
+  "absolute -top-2 h-16 border-2 border-r border-black/50";
+
+export const currentValueStyle =
+  "absolute top-14 text-lg font-bold text-black/50";
+
+export const maxValueStyle =
+  "w-full p-2 text-right text-lg font-bold text-black/50 relative z-10";
+
+const Wrapper = createComponent("div", tv({ base: wrapperStyle }));
+const ProgressBar = createComponent("div", tv({ base: progressBarStyle }));
+const Indicator = createComponent("div", tv({ base: indicatorStyle }));
+const MaxValue = createComponent("div", tv({ base: maxValueStyle }));
+const CurrentValue = createComponent("div", tv({ base: currentValueStyle }));
--- apps/web/src/pages/api/og.tsx
+++ apps/web/src/pages/api/og.tsx
@@ -1,6 +1,14 @@
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
-import { poolMetadata } from "~/data/mock";
+import { RadialSVG } from "~/components/RadialSVG";
+import {
+  currentValueStyle,
+  indicatorStyle,
+  maxValueStyle,
+  progressBarStyle,
+  wrapperStyle,
+} from "~/components/RaisedProgress";
+import { pool, poolMetadata } from "~/data/mock";
 import { formatMoney } from "~/utils/currency";
 
 export const config = {
@@ -19,20 +27,18 @@ async function getPoolDetails(address: string) {
 export default async function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
-    console.log(req.url, Object.entries(searchParams));
     const poolAddress = searchParams.get("poolAddress");
-    console.log({ poolAddress });
     if (!poolAddress) return null;
 
-    const pool = await getPoolDetails(poolAddress);
+    const { goal } = pool;
 
-    const {
-      metadata: { title, description },
-      raised,
-      goal,
-    } = pool;
-    const value = raised / goal;
-    const symbol = "$";
+    // Fetch from ipfs
+    const { title, description } = poolMetadata;
+
+    // Fetch from subgraph
+    const raised = "15000";
+
+    const percentage = `${(+raised / +goal) * 100}%`;
 
     return new ImageResponse(
       (
@@ -47,32 +53,50 @@ export default async function handler(req: NextRequest) {
             backgroundColor: "white",
           }}
         >
-          <div tw="bg-gray-50 w-full h-full flex flex-col justify-between p-16">
-            <h2 tw="flex flex-col text-gray-900">
-              <span tw="text-6xl tracking-tight mb-6">{title}</span>
-              <span tw="text-3xl leading-10">{description}</span>
-            </h2>
-            <div tw="flex flex-col">
-              <div tw="flex justify-between">
-                <Stats value={formatMoney(raised, "USD")} label="Raised" />
-                <Stats value={formatMoney(goal, "USD")} label="Goal" />
+          <div tw="bg-[#FFE767] w-full h-full flex flex-col justify-between relative">
+            <div
+              tw="flex absolute"
+              style={{
+                width: 444,
+                height: 845,
+              }}
+            >
+              <RadialSVG />
+            </div>
+            <div tw="flex flex-col pl-64 pr-32 pt-32">
+              <div tw="flex flex-col mb-8">
+                <span tw="mb-4 text-6xl font-black uppercase text-black/70">
+                  {title}
+                </span>
+                <span tw="text-2xl">{description}</span>
               </div>
-              <div tw="h-4 bg-green-200 flex">
-                <div
-                  tw="bg-green-600 h-full"
-                  style={{ width: `${value * 100}%` }}
-                />
+
+              <div tw={wrapperStyle} style={{}}>
+                <span tw={progressBarStyle} style={{ width: percentage }} />
+                <span tw={indicatorStyle} style={{ left: percentage }} />
+                <span
+                  tw={currentValueStyle}
+                  style={{ left: percentage, transform: `translateX(-50%)` }}
+                >
+                  {formatMoney(raised)} Raised
+                </span>
+                <span tw={maxValueStyle}>{formatMoney(goal)} Goal</span>
               </div>
             </div>
-            <span tw="text-right w-full">
-              Last updated: {new Date().toLocaleString()}
-            </span>
+            <div tw="flex justify-end p-1">
+              <span>Last updated: {new Date().toLocaleString()}</span>
+            </div>
           </div>
         </div>
       ),
       {
         width: 1200,
         height: 630,
+        // debug: true,
+        // fonts: [{
+        //   name: "Inter",
+        //   data: null
+        // }]
       }
     );
   } catch (e: any) {
