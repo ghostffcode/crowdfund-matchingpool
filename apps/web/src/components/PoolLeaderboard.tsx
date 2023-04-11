b97af4b2ce789631919fe6d1ab5e5b936373a9b2
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-04-11 11:32:40 +0200

    Add text if leaderboard is empty

--- apps/web/src/components/PoolLeaderboard.tsx
+++ apps/web/src/components/PoolLeaderboard.tsx
@@ -8,27 +8,37 @@ export const Leaderboard = ({ donations = [] }: { donations: any[] }) => {
     <section>
       <h4 className="mb-2 text-xl font-bold">Leaderboard</h4>
       <div className="mb-4 flex flex-col divide-y divide-solid">
-        {donations.map((donation) => (
-          <div
-            key={donation.user.address}
-            className="flex  border-black/80 py-6"
-          >
-            <EnsAvatar address={donation.user.address} size="sm" color="gray" />
-            <div className="flex flex-1 items-center justify-between pl-4">
-              <EnsName address={donation.user.address} />
-              <div className="">${formatMoney(donation.amount)}</div>
+        {!donations.length ? (
+          <div className="text-center">No contributions yet</div>
+        ) : (
+          donations.map((donation) => (
+            <div
+              key={donation.user.address}
+              className="flex  border-black/80 py-6"
+            >
+              <EnsAvatar
+                address={donation.user.address}
+                size="sm"
+                color="gray"
+              />
+              <div className="flex flex-1 items-center justify-between pl-4">
+                <EnsName address={donation.user.address} />
+                <div className="">${formatMoney(donation.amount)}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
       <div className="flex justify-center">
-        <Button
-          className="w-72"
-          onClick={() => alert("not implemented")}
-          variant="ghost"
-        >
-          Load more
-        </Button>
+        {donations.length ? (
+          <Button
+            className="w-72"
+            onClick={() => alert("not implemented")}
+            variant="ghost"
+          >
+            Load more
+          </Button>
+        ) : null}
       </div>
     </section>
   );
