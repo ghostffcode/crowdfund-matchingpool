e31acbe57aac20c51755ddc21b138fc4a8b87d84
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-04-14 11:33:03 +0200

    Add connect wallet button and redirect when Crowdfund created

--- apps/web/src/hooks/useCrowdfundCreate.ts
+++ apps/web/src/hooks/useCrowdfundCreate.ts
@@ -1,12 +1,21 @@
 import { useContractWrite } from "wagmi";
+import { SendTransactionResult } from "@wagmi/core";
 import { useContractConfig } from "./useContractConfig";
+import { ethers } from "ethers";
 
-export function useCrowdfundCreate() {
+export function useCrowdfundCreate({
+  onSuccess,
+}: {
+  onSuccess: (data: SendTransactionResult) => void;
+}) {
   const CrowdfundFactory = useContractConfig("CrowdfundFactory");
-
-  return useContractWrite({
-    ...CrowdfundFactory,
-    mode: "recklesslyUnprepared",
-    functionName: "createCrowdfund",
-  });
+  return {
+    ...useContractWrite({
+      ...CrowdfundFactory,
+      mode: "recklesslyUnprepared",
+      functionName: "createCrowdfund",
+      onSuccess,
+    }),
+    iface: new ethers.utils.Interface(CrowdfundFactory.abi),
+  };
 }
